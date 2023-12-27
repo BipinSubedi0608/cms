4d86b4b70fe76a85ca44bd84d1dfe522ac77152b
BipinSubedi0608 <subedibipin69@gmail.com>
2023-12-27 22:14:24 +0545

    Merged Successfully

--- Models/foods.js
+++ Models/foods.js
@@ -37,33 +37,4 @@ const foods = {
     5: puri
 };
 
-$(document).ready(function () {
-
-    $(".foodContainerDiv").hover(
-        function() {
-            var img = $(this).find('img');
-            var btn = $(this).find('button');
-            img.addClass('scale-110');
-            btn.addClass('scale-110');
-        },
-        function() {
-            var img = $(this).find('img');
-            img.removeClass('scale-110');
-            var btn = $(this).find('button');
-            btn.removeClass('scale-110');
-        }
-    );
-
-    var foodImg = document.getElementsByClassName("foodImg");
-    var foodName = document.getElementsByClassName("foodName");
-    var foodQuantity = document.getElementsByClassName("foodQuantity");
-    var foodPrice = document.getElementsByClassName("foodPrice");
-
-    for (i = 0; i < 5; i++)
-    {
-        foodImg[i].src = foods[i+1].imgSrc;
-        foodName[i].innerHTML = foods[i+1].name;
-        foodQuantity[i].innerHTML = foods[i+1].quantity;
-        foodPrice[i].innerHTML = foods[i+1].price;
-    }
-});
+export default foods;
